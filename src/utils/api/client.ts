70deba452aedc77a4d991212e44ccec1de63bfea
sanggit999 <sanggit999@gmail.com>
2026-05-21 09:55:03 +0700

    SK current state before HRM UI refactor

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
-export const API_BASE_URL = 'https://sk-hrm.amcare.vn';
+//export const API_BASE_URL = 'https://sk-hrm.amcare.vn';
 //export const API_BASE_URL = 'https://app-uat.amcare.vn';
-//export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'http://localhost:8000';
 // Create axios instance for Management API
 export const managementApi: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
